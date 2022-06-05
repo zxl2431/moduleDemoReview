074a4fdb05472de80567d392cf09357a8cb94b14
zhangxianlin <zhangxianlin2431@163.com>
2022-06-05 23:24:36 +0800

    数组的一般性操作

--- JavaBaseTest01/src/main/java/cn/agree/lesson/ArrayDefine.java
+++ JavaBaseTest01/src/main/java/cn/agree/lesson/ArrayDefine.java
@@ -14,6 +14,24 @@ public class ArrayDefine {
 
         System.out.println("arr1的长度:"+arr1.length+". arr2的长度:"+arr2.length+". arr3的长度:"+ arr3.length);
 
+        // 数组元素的赋值和修改
+        arr1[0] = 5;
+        arr1[1] = 6;
+        arr1[2] = 7;
+
+        System.out.println(arr1[1]);
+
+        int[] arrtemp = arr1; // 这个arrtemp指向了arr1
+        arr1[1] = 88;
+        System.out.println(arr1[1]);
+
+        // 数组的常规操作比较简单 记住索引就行了
+        // 常见的异常是数组越界异常和空指针异常
+
+        // 但是数组是和算法连接比较紧密的 最数组的最大值 数组的排序 数组的反转
+
+        // 数组名 作为方法参数和返回值
+
 
     }
 }
