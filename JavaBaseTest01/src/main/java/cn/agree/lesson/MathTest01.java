064d3375df6135f3e7abc2026ff6eeaac81dfa6c
zhangxianlin <zhangxianlin2431@163.com>
2022-06-06 23:29:53 +0800

    Math类的abs绝对值 ceil floor round方法

---
+++ JavaBaseTest01/src/main/java/cn/agree/lesson/MathTest01.java
@@ -0,0 +1,19 @@
+package cn.agree.lesson;
+
+public class MathTest01 {
+    public static void main(String[] args) {
+
+        // 绝对值
+        System.out.println(Math.abs(-5));
+
+        // 大于参数的最小整数
+        System.out.println(Math.ceil(3.3));
+
+        // 小于参数的最大整数
+        System.out.println(Math.floor(3.3));
+
+        // 四舍五入
+        System.out.println(Math.round(5.4));
+        System.out.println(Math.round(5.5));
+    }
+}
