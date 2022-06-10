c4406ea58f510d5d8de40614a5c30ac6e59eaa31
zhangxianlin <zhangxianlin2431@163.com>
2022-06-10 23:20:20 +0800

    线程的第二种创建方式 实现implements Runable接口

---
+++ JavaBaseTest01/src/main/java/cn/agree/Thread/ThreadDemo02.java
@@ -0,0 +1,14 @@
+package cn.agree.Thread;
+
+public class ThreadDemo02 {
+    public static void main(String[] args) {
+        // 创建自定义类对象 线程任务对象
+        MyRunnable mr = new MyRunnable();
+        // 创建线程对象
+        Thread t = new Thread(mr, "小强");
+        t.start();
+        for (int i=0; i<20; i++) {
+            System.out.println("旺财"+i);
+        }
+    }
+}
