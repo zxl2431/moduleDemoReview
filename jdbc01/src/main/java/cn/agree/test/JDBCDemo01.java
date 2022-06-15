df4df981231adc1c4c331a1e29753815a4c86842
zhangxianlin <zhangxianlin2431@163.com>
2022-06-16 00:26:42 +0800

    注册Driver -> DriverManager -> Connnetion -> Statement -> ResultSet

--- jdbc01/src/main/java/cn/agree/test/JDBCDemo01.java
+++ jdbc01/src/main/java/cn/agree/test/JDBCDemo01.java
@@ -1,9 +1,6 @@
 package cn.agree.test;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCDemo01 {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -12,9 +9,33 @@ public class JDBCDemo01 {
         // 类加载器 会这个类的静态代码块
         Class.forName("com.mysql.jdbc.Driver");
 
+        // 获取连接
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "root");
-
         System.out.println(connection);
+
+        // 获取Statement对象
+        Statement statement = connection.createStatement();
+
+        /*// 插入表
+        String sql = "insert into category (cname) values ('手机');";
+        int i = statement.executeUpdate(sql);
+
+        System.out.println("影响行数:"+i);*/
+
+        queryCategory(statement);
+
+    }
+
+    public static void queryCategory(Statement statement) throws SQLException {
+        String sql = "select * from category;";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            int cid = resultSet.getInt("cid");
+            String cname = resultSet.getString("cname");
+            System.out.println(cid + "=="+cname);
+        }
 
     }
 }
