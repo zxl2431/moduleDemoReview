2060bf96efd885d47e5bc33e11eedc9d8e9088f9
zhangxianlin <zhangxianlin2431@163.com>
2022-06-18 23:30:35 +0800

    JdbcTemplate 的update用于插入,修改,删除数据

--- jdbc01/src/main/java/cn/agree/template/TemplateDemo01.java
+++ jdbc01/src/main/java/cn/agree/template/TemplateDemo01.java
@@ -7,7 +7,17 @@ public class TemplateDemo01 {
     public static void main(String[] args) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(DruidUtils.getDataSource());
 
-        createTable(jdbcTemplate);
+        // 建表
+        // createTable(jdbcTemplate);
+
+        // 插入数据
+        // test01(jdbcTemplate);
+
+        // 修改数据
+        // test02(jdbcTemplate);
+
+        // 删除数据
+        test03(jdbcTemplate);
 
     }
 
@@ -21,5 +31,50 @@ public class TemplateDemo01 {
         template.execute(sql);
 
     }
+
+    /*
+    *  添加数据
+    *
+    *
+    * */
+    public static void test01(JdbcTemplate template) {
+        String sql = "insert into product values (null, ?,?);";
+
+        template.update(sql, "iPhone3GS", 3333);
+        template.update(sql, "iPhone4", 5000);
+        template.update(sql, "iPhone4S", 5001);
+        template.update(sql, "iPhone5", 5555);
+        template.update(sql, "iPhone5C", 3888);
+        template.update(sql, "iPhone5S", 5666);
+        template.update(sql, "iPhone6", 6666);
+        template.update(sql, "iPhone6S", 7000);
+        template.update(sql, "iPhone6SP", 7777);
+        template.update(sql, "iPhoneX", 8888);
+    }
+
+    /*
+    *  修改数据
+    *
+    * */
+    public static void test02(JdbcTemplate template) {
+        String sql = "update product set pname=?, price=? where pid =?;";
+
+        int i = template.update(sql, "XVIII", 18888, 10);
+        System.out.println("影响的行数:"+i);
+
+    }
+
+    /*
+    *  删除数据
+    *
+    * */
+    public static void test03(JdbcTemplate template) {
+        String sql = "delete from product where pid=?;";
+
+        int i = template.update(sql, 7);
+        System.out.println("影响的行数:"+i);
+    }
+
+
 
 }
