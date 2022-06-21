3e5c1016cf0960a08da17cc87fe27ac702f2aaff
zhangxianlin <zhangxianlin2431@163.com>
2022-06-22 00:46:01 +0800

    ServletContext的使用

---
+++ rsp/src/cn/agree/context/ConServlet01.java
@@ -0,0 +1,26 @@
+package cn.agree.context;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ConServlet01", urlPatterns = "/con01")
+public class ConServlet01 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取容器
+        ServletContext context = getServletContext();
+        //存入数据
+        context.setAttribute("addr","广州");
+    }
+}
