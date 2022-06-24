032fba16ceda899af073aff485841710eafb994d
zhangxianlin <zhangxianlin2431@163.com>
2022-06-25 00:15:34 +0800

    Filter路径的映射 模糊匹配 精准匹配

--- filter/src/cn/agree/filter/FilterDemo02.java
+++ filter/src/cn/agree/filter/FilterDemo02.java
@@ -5,23 +5,21 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(filterName = "FilterDemo01", urlPatterns = "/*")
-public class FilterDemo01 implements Filter {
-
+@WebFilter(filterName = "FilterDemo02", urlPatterns = "/*.jsp")
+public class FilterDemo02 implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("FilterDemo01过滤器初始化...");
+        System.out.println("FilterDemo02初始化了...");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("FilterDemo01执行了...");
-        // 放行
+        System.out.println("FilterDemo02放行了...");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
     public void destroy() {
-        System.out.println("FilterDemo01过滤器销毁了...");
+        System.out.println("FilterDemo02销毁了");
     }
 }
