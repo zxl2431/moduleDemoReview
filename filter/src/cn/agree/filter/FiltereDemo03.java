1afb998e4a97e1816030cae6f11d7261f0bc84f4
zhangxianlin <zhangxianlin2431@163.com>
2022-06-25 00:26:57 +0800

    过滤器的拦截方式 Request请求和forward请求转发

--- filter/src/cn/agree/filter/FiltereDemo03.java
+++ filter/src/cn/agree/filter/FiltereDemo03.java
@@ -1,25 +1,24 @@
 package cn.agree.filter;
 
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter(filterName = "FilterDemo02", urlPatterns = "/*.jsp")
-public class FilterDemo02 implements Filter {
+@WebFilter(filterName = "FiltereDemo03", urlPatterns = "/*",
+        dispatcherTypes = {DispatcherType.FORWARD, DispatcherType.REQUEST})
+public class FiltereDemo03 implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("FilterDemo02初始化了...");
+        System.out.println("FiltereDemo03初始化了...");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("FilterDemo02放行了...");
-        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("FiltereDemo03的doFilter执行了...");
     }
 
     @Override
     public void destroy() {
-        System.out.println("FilterDemo02销毁了...");
+        System.out.println("FilterDemo03销毁了...");
     }
 }
