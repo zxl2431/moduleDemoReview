2f0482090c3a6cba9cdebc81e5cfcaf06ba0f602
zhangxianlin <zhangxianlin2431@163.com>
2022-06-26 00:33:52 +0800

    Maven工程WebApp骨架测试1

---
+++ mav_test/src/test/java/cn/agree/test/TestDemo01.java
@@ -0,0 +1,11 @@
+package cn.agree.test;
+
+import org.junit.Test;
+
+public class TestDemo01 {
+
+    @Test
+    public void test01() {
+        System.out.println("maven command test 测试成功...");
+    }
+}
